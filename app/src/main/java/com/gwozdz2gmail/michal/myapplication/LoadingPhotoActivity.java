32df21299354d678eb920c9dd264bde677eb91c2
olek95 <olek1995s@poczta.onet.pl>
2017-03-31 11:26:42 +0200

    two ideas for saving photos

--- app/src/main/java/com/gwozdz2gmail/michal/myapplication/LoadingPhotoActivity.java
+++ app/src/main/java/com/gwozdz2gmail/michal/myapplication/LoadingPhotoActivity.java
@@ -25,6 +25,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class LoadingPhotoActivity extends AppCompatActivity {
 
@@ -121,7 +123,53 @@ public class LoadingPhotoActivity extends AppCompatActivity {
     }
 
     public void save(View view) {
-        MediaStore.Images.Media.insertImage(getContentResolver(), ((BitmapDrawable)image.getDrawable()).getBitmap(),"MyFile.png", "Created file");
-        Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show();
+        //It's first idea. It's working but we can't create our folder, because it creates default folder Pictures in Gallery
+        /*MediaStore.Images.Media.insertImage(getContentResolver(), ((BitmapDrawable)image.getDrawable()).getBitmap(),"MyFile.png", "Created file");
+        Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show();*/
+
+        /* This is where the second idea begins. It's better idea, because we can create our
+        folder for this application for example OurApplicationFolder and this folder will be created
+        in Gallery. But be careful - saving process is strange - to displayed new folder you must for
+        example disconnect cabel from computer or restart phone and wait.
+         */
+        Bitmap map = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            Log.d("TAG",
+                    "Error creating media file, check storage permissions: ");// e.getMessage());
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            map.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+            Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Log.d("TAG", "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d("TAG", "Error accessing file: " + e.getMessage());
+        }
+    }
+    private  File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/thisFolder");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName="MI_"+ timeStamp +".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
     }
 }
