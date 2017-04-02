a16cc8544b3b2b2cd2ec75445268ecc12e55afc5
olek95 <olek1995s@poczta.onet.pl>
2017-04-02 22:17:11 +0200

    added about dialog, added code to showPopup method in ProgramManager, deleted unnecessary strings in strings.xml

--- app/src/main/java/com/gwozdz2gmail/michal/myapplication/MainActivity.java
+++ app/src/main/java/com/gwozdz2gmail/michal/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
--- app/src/main/java/com/gwozdz2gmail/michal/myapplication/ProgramManager.java
+++ app/src/main/java/com/gwozdz2gmail/michal/myapplication/ProgramManager.java
@@ -11,9 +11,12 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -41,10 +44,37 @@ public class ProgramManager {
      * @param activity activity
      * @param view menu's owner
      */
-    public static void showPopup(Activity activity, View view){
+    public static void showPopup(final Activity activity, View view){
         PopupMenu popup = new PopupMenu(activity, view);
         MenuInflater inflater = popup.getMenuInflater();
         inflater.inflate(R.menu.settings_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.language:
+                        return true;
+                    case R.id.about:
+                        showAboutDialog(activity);
+                        return true;
+                    case R.id.help:
+                        return true;
+                }
+                return false;
+            }
+        });
         popup.show();
+    }
+
+    private static void showAboutDialog(Activity activity){
+        Resources resources = activity.getResources();
+        TextView about = new TextView(activity);
+        about.setText(resources.getString(R.string.authors) + "\n" + resources.getString(R.string.version));
+        about.setGravity(Gravity.CENTER_HORIZONTAL);
+        about.setPadding(0, 30, 0, 0);
+        AlertDialog dialog = new AlertDialog.Builder(activity).setTitle(resources.getString(R.string.about))
+                .setView(about).setPositiveButton(resources.getString(R.string.ok), null).create();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
     }
 }
