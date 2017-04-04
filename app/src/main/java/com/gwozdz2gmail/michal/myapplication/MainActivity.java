6a300820e733c7065cce93c02296b64d88d39a2c
Łysy <zuliq94@gmail.com>
2017-04-04 15:02:42 +0200

    settings update

--- app/src/main/java/com/gwozdz2gmail/michal/myapplication/MainActivity.java
+++ app/src/main/java/com/gwozdz2gmail/michal/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gwozdz2gmail.michal.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
@@ -12,8 +13,9 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity{
+    private int visible = View.INVISIBLE;
     private static final String TAG = "AndroidCameraApi";
-    private ImageButton takePictureButton;
+    private ImageButton takePictureButton, settingsButton, filtersButton, shareButton, languagesButton, creditsButton, bugButton;
     protected static TextureView textureView;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
     private Camera2API camera2API;
@@ -22,16 +24,86 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        camera2API = new Camera2API(MainActivity.this);
+
+        initViewElements();
+        initListeners();
+    }
+
+    private void initViewElements() {
         textureView = (TextureView) findViewById(R.id.texture);
         textureView.setSurfaceTextureListener(textureListener);
 
-        camera2API = new Camera2API(MainActivity.this);
-
         takePictureButton = (ImageButton) findViewById(R.id.btn_take_picture);
+        settingsButton = (ImageButton) findViewById(R.id.settings);
+        filtersButton = (ImageButton) findViewById(R.id.filters);
+        shareButton = (ImageButton) findViewById(R.id.share);
+        languagesButton = (ImageButton) findViewById(R.id.languages);
+        creditsButton = (ImageButton) findViewById(R.id.credits);
+        bugButton = (ImageButton) findViewById(R.id.bugs);
+    }
+
+    private void initListeners() {
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 camera2API.takePicture();
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(visible == View.INVISIBLE) {
+                    visible = View.VISIBLE;
+                } else {
+                    visible = View.INVISIBLE;
+                }
+
+                filtersButton.setVisibility(visible);
+                shareButton.setVisibility(visible);
+                languagesButton.setVisibility(visible);
+                creditsButton.setVisibility(visible);
+                bugButton.setVisibility(visible);
+            }
+        });
+
+        filtersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FILTERS", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SHARE", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        languagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "LANGUAGES", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        creditsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "CREDITS", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, CreditsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bugButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "BUG REPORTS", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, BugReportActivity.class);
+                startActivity(intent);
             }
         });
     }
